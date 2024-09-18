76ee57a77120e0cdf6c431e60bcf6847ec012364
Himanshu Masant <himanshu.masant@scoobies.ai>
2024-09-18 19:22:46 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/finance_help_mate/finance_help_mate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.finance_help_mate.finance_help_mate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Finance Help Mate")),
+        body: const Center(child: Text("Finance Help Mate")),
+      ),
+    );
+  }
+}
